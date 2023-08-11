52aa67326859cfb56cdd5a8f0d0757464328b6eb
Reem122 <reemoahmed2002@gmail.com>
2023-08-11 18:28:36 +0300

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: task 2 - Variables, if, else, while - alx
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+	sprintf(arr, "%d", n);
+	
+	for (c='a'; c='z'; c++)
+	{
+		putchar(c);
+		putchar('\n');
+	}
+	
+	return (0);
+}
