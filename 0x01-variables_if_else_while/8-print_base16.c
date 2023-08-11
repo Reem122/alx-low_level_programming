19fdc8fffb734abc2638072f452a97deff029a6f
Reem122 <reemoahmed2002@gmail.com>
2023-08-11 20:37:02 +0300

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: task 2 - Variables, if, else, while - alx
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+	}
+	for (c = 65; c <= 70; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
