a15f95af6295e7a8aca7f4c2aa956ae886d61003
Reem122 <reemoahmed2002@gmail.com>
2023-08-16 02:19:20 +0300

    0-putchar.c

---
+++ 0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: task 0 - Functions, nested loops - alx
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+	char word[8] = "_putchar";
+	for(n = 0; n < 8; n++)
+	{
+		putchar(word[i]);
+	}
+	putchar("\n");
+	return (0);
+}
